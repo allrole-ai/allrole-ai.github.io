c779334e5a17d0687664146176d5c5e6aa637bf2
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-08 00:49:13 +0700

    add link API login di template

--- js/template/template.js
+++ js/template/template.js
@@ -4,6 +4,6 @@
 export let URLRegister = ``
 
 //url login 
-export let URLLogin = ``
+export let URLLogin = `https://asia-southeast2-allrole.cloudfunctions.net/allrole/login`
 
 export let token = 'token';
