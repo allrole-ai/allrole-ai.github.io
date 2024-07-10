8af8fc2512c2f257ef84abb1da72b1ab5cbea174
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-11 04:40:14 +0700

    Update controller.js to handle form submission and data posting

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,13 +1,15 @@
-import {postWithBearer} from "https://jscroot.github.io/api/croot.js";
-import {GetDataForm,ResponsePost} from "../config/config.js";
-import {token,URLRegister} from "../template/template.js";
+import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
+import { GetDataForm, ResponsePost } from "../config/config.js";
+import { token, URLRegister } from "../template/template.js";
 
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("#formregis");
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-        let data = GetDataForm();
-        postWithBearer(URLRegister, token, data, ResponsePost)
+        if (validateForm('sign-up')) {
+            let data = GetDataForm();
+            postWithBearer(URLRegister, token, data, ResponsePost)
+        }
     });
 });
