231d029005b917354bbdf31e5f08f39482b8a625
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-23 23:11:44 +0700

    add Response

--- js/controller/controllerLogin.js
+++ js/controller/controllerLogin.js
@@ -10,7 +10,3 @@ document.addEventListener("DOMContentLoaded", function () {
         postWithBearer(URLLogin, token, data, ResponseLogin);
     });
 });
-
-// import {postWithBearer} from "https://jscroot.github.io/api/croot.js";
-// import {PostLogin, ResponseLogin} from "../config/config.js";
-// import {token, URLLogin} from "../template/template.js";
