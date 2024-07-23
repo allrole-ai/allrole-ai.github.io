baa157b5e518fd5c87f6b27e9a179d93d34e92b5
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-23 23:11:23 +0700

    add Responsejs

--- js/controller/controllerLogin.js
+++ js/controller/controllerLogin.js
@@ -14,14 +14,3 @@ document.addEventListener("DOMContentLoaded", function () {
 // import {postWithBearer} from "https://jscroot.github.io/api/croot.js";
 // import {PostLogin, ResponseLogin} from "../config/config.js";
 // import {token, URLLogin} from "../template/template.js";
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const form = document.getElementById("formlogin");
-//     form.addEventListener("submit", function (event) {
-//         event.preventDefault(); // Mencegah form melakukan submit default
-//         let data = PostLogin(); // Mengambil data login dari fungsi PostLogin
-//         postWithBearer(URLLogin, token, data, ResponseLogin); // Mengirim data login ke server menggunakan token untuk otentikasi
-//     });
-// });
-
-// // Fungsi ResponseLogin untuk menangani respons dari server setelah login
