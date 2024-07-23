93a29f9fc788a6e31f5960041f9d1e9351b4f2cb
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-23 23:30:21 +0700

    remove checklogin

--- js/checklogin.js
+++
@@ -1,6 +0,0 @@
-import {postWithBearer} from "https://jscroot.github.io/api/croot.js";
-import {PostLogin, ResponseLogin} from "../config/config.js";
-import {token, URLLogin} from "../template/template.js";
-
-
-
