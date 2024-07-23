b2d97e94a4cb9ada6e5a625a76328515299e949e
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-07-23 23:11:03 +0700

    add Response

--- js/controller/controllerLogin.js
+++ js/controller/controllerLogin.js
@@ -25,13 +25,3 @@ document.addEventListener("DOMContentLoaded", function () {
 // });
 
 // // Fungsi ResponseLogin untuk menangani respons dari server setelah login
-// // Parameter response berisi data yang dikembalikan oleh server setelah proses login
-// function ResponseLogin(response) {
-//     if (response.ok) {
-//         // Jika respons sukses, misalnya status HTTP 200
-//         // Logika untuk menangani login berhasil, misalnya menyimpan token dan mengarahkan pengguna ke halaman utama
-//         console.log("Login berhasil:", response.data);
-//         // Simpan token ke localStorage atau sessionStorage
-//         localStorage.setItem('authToken', response.data.token);
-//         // Arahkan pengguna ke halaman dashboard
-
