c5a9d8489052b012780d0279b68626e2dc3a948b
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-27 01:56:51 +0700

    Add constants for API endpoints and table configuration

---
+++ js/src/config.js
@@ -0,0 +1,9 @@
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+export const URLChat = "https://backend-production-9c11.up.railway.app/chat";
+export const UrlLogin = "https://backend-production-9c11.up.railway.app/login";
+export const UrlRegister = "https://backend-production-9c11.up.railway.app/register";
+export const tableTag = "tr";
+export const tableRowClass = "content is-small";
+export const token = "tokennya";
+export const cookie = getCookie("Login");
