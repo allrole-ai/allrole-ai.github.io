46d653beca550697e6665104308102723eaf6455
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-27 01:59:59 +0700

    Update API endpoint constants for chat, login, and registration

--- js/template/template.js
+++ js/template/template.js
@@ -1,9 +1,13 @@
 // template js
 
 //url register 
-export let URLRegister = `https://asia-southeast2-allrole.cloudfunctions.net/allrole/register`
+// export let URLRegister = `https://asia-southeast2-allrole.cloudfunctions.net/allrole/register`
 
-//url login 
-export let URLLogin = `https://asia-southeast2-allrole.cloudfunctions.net/allrole/login`
+// //url login 
+// export let URLLogin = `https://asia-southeast2-allrole.cloudfunctions.net/allrole/login`
+
+export const URLChat = "https://backend-production-9c11.up.railway.app/chat";
+export const URLLogin = "https://backend-production-9c11.up.railway.app/login";
+export const URLRegister = "https://backend-production-9c11.up.railway.app/register";
 
 export let token = 'token';
