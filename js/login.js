a9118f0bbe6482dad59bbb04c5ac3a190df9dd5c
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-27 01:52:29 +0700

    update login.js

--- js/login.js
+++ js/login.js
@@ -1,34 +1,14 @@
-let container = document.getElementById('container');
+import { handleLogin } from '../js/src/controller.js';
 
-function toggleForm(formType) {
-    container.classList.toggle('sign-in', formType === 'sign-in');
-    container.classList.toggle('sign-up', formType === 'sign-up');
-}
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("formlogin");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    handleLogin();
+  });
 
-// Pastikan bahwa fungsi validateForm tidak lagi menampilkan alert tetapi memvalidasi form
-function validateForm(type) {
-    let username = document.querySelector(`.${type} input[placeholder="Nama Lengkap"]`).value;
-    let password = document.querySelector(`.${type} input[placeholder="Password"]`).value;
-    if (type === 'sign-up') {
-        let email = document.querySelector(`.${type} input[placeholder="Email"]`).value;
-        let confirmPassword = document.querySelector(`.${type} input[placeholder="Confirm your Password"]`).value;
-        if (!username || !email || !password || !confirmPassword) {
-            alert('Please fill all the fields');
-            return false;
-        }
-        if (password !== confirmPassword) {
-            alert('Passwords do not match');
-            return false;
-        }
-    } else {
-        if (!username || !password) {
-            alert('Please fill all the fields');
-            return false;
-        }
-    }
-    return true;
-}
+  // Menampilkan atau menyembunyikan password
 
-setTimeout(() => {
-    container.classList.add('sign-in');
-}, 200);
+    const passwordField = document.getElementById("password");
+    passwordField.type = this.checked ? "text" : "password";
+  });
