5bfeb83fb464c8fc853d8443619b7f742b4f901d
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-29 22:19:40 +0700

    Delete JS coundown

--- js/home.js
+++ js/home.js
@@ -9,29 +9,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-function countdownTimer() {
-    const endDate = new Date("December 31, 2024 23:59:59").getTime();
-    const now = new Date().getTime();
-    const timeLeft = endDate - now;
-
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-    if (timeLeft < 0) {
-        clearInterval(timerInterval);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-    }
-}
-
-const timerInterval = setInterval(countdownTimer, 1000);
-
-
-
 
